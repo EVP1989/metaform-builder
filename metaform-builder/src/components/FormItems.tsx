0c54a11aedbcf88defaa2c0a95e48ca9b154b991
Eetu Lepistö <eetu.lepisto@metatavu.fi>
2021-01-21 19:48:11 +0200

    Added typing and some  styling

--- metaform-builder/src/components/FormItems.tsx
+++ metaform-builder/src/components/FormItems.tsx
@@ -1,53 +1,88 @@
+import { Box, createStyles, List, ListItem, ListItemIcon, ListItemText, makeStyles, Theme } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import SubjectIcon from '@material-ui/icons/Subject';
 import React, { useState, useRef } from 'react';
 import '../styles/FormItems.css';
-//TODO: Correct type definitions (useRef, )!!
 
-//interface FormItems
+//DnD items based on github repo: https://github.com/Gaurav2048/React-DnD-Adv
 
-const FormItems = () => {
+//Styles
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      maxWidth: 250,
+    },
+    components: {
+      backgroundColor: theme.palette.background.default,
+        
+    },
+    icon: {
+      margin: theme.spacing(1, 2, 1),
+    },
+  }),
+);
 
-const draggingItem = useRef<any | null>(null);
-const dragOverItem = useRef<any | null>(null);
+const FormItems : React.FC = () => {
 
-const [list, setList] = useState([
-'item-1',
-'item-2',
-'item-3'
-]);
+    const classes = useStyles();
+    const draggingItem = useRef<number | null>(null);
+    const dragOverItem = useRef<number | null>(null);
 
-const handleDragStart = (e : any, position :any) => {
-    draggingItem.current = position;
-};
+    //TODO: Generate list from static list of form components
+    const [formBlockList, setFormBlockList] = useState([
+    'Tekstikenttä1',
+    'Tekstikenttä2',
+    'Tekstikenttä3'
+    ]);
 
-const handleDragEnter = (e : any, position : any) => {
-    dragOverItem.current = position;
-};
+    const handleDragStart = (e : any, position :number) => {
+        draggingItem.current = position;
+    };
 
-const handleDragEnd = (e : any) => {
-const listCopy = [...list];
-const draggingItemContent = listCopy[draggingItem.current];
-listCopy.splice(draggingItem.current, 1);
-listCopy.splice(dragOverItem.current, 0, draggingItemContent);
-    
-draggingItem.current = null;
-dragOverItem.current = null;
-setList(listCopy);
-};
+    const handleDragEnter = (e : any, position : number) => {
+        dragOverItem.current = position;
+        const newFormBlockList = [...formBlockList];
+        if(draggingItem.current != null) {
+            const draggingItemContent = newFormBlockList[draggingItem.current];
+            newFormBlockList.splice(draggingItem.current, 1);
+            newFormBlockList.splice(dragOverItem.current, 0, draggingItemContent);
+        }
+            
+        draggingItem.current = dragOverItem.current;
+        dragOverItem.current = null;
+        setFormBlockList(newFormBlockList);
+        console.log(newFormBlockList);
+    };
 
-return (
-<>
-{
- list &&
-  list.map((item, index) => (
-     <h1 
-        onDragStart={(e) => handleDragStart(e, index)}
-        onDragEnter={(e) => handleDragEnter(e, index)}
-        onDragEnd={handleDragEnd}
-        key={index} draggable>
-       {item}
-     </h1>
-    ))}
-   </>
-  );
+    return (
+    <>
+    <Grid>
+        <List className={classes.root}>
+            {formBlockList && formBlockList.map((item, index) => (
+            <ListItem
+                onDragStart={(e) => handleDragStart(e, index)}
+                onDragEnter={(e) => handleDragEnter(e, index)}
+                onDragOver={(e) => e.preventDefault()}
+                key={index} draggable
+                className={classes.components}
+                >
+                <Box border={1} display="flex" pr={2}>
+                    <ListItemIcon className={classes.icon}>
+                        <SubjectIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                        <Typography variant="h6">
+                            {item}
+                        </Typography>
+                    </ListItemText>
+                </Box>
+            </ListItem>
+            ))}
+        </List> 
+    </Grid>
+    </>
+    );
 };
 export default FormItems;
