04e656152e77bbff965208b265df0f4bca5ae136
Eetu Lepistö <eetu.lepisto@metatavu.fi>
2021-02-03 13:59:40 +0200

    Simple json preview with example metaformJson

--- metaform-builder/src/components/JsonPreview.tsx
+++ metaform-builder/src/components/JsonPreview.tsx
@@ -1,10 +1,11 @@
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import React from 'react';
-import '../styles/FormItems.css';
+import React, { useState } from 'react';
+import MetaformTypes from '../types/metaForm';
+import metaformExampleJson from '../model/metaformExampleJSON';
 
 interface Props {
-
+    json : MetaformTypes
 }
 
 /**
@@ -13,13 +14,34 @@ interface Props {
  */
 const Preview : React.FC<any> = (props : Props) => {
 
+    const [metaformJson, setMetaformJson] = useState<any>(metaformExampleJson)
+
   return (
   <>
-  <Grid item md={12}>
-    <Typography variant="h4" >
-      Jsonin esikatselu, lataus ja mahdollinen käpistely(?)
-    </Typography> 
+  <Grid container >
+    <Grid item md={12}>
+        <Typography variant="h4" >
+            Jsonin esikatselu, lataus ja mahdollinen käpistely(?)
+        </Typography> 
+    </Grid>
+    <Grid item md={3} >
+  
+    </Grid>
+    <Grid item md={6}>
+        <Box>
+            <pre style={{backgroundColor: '#eee'}}>
+                <h2>Esimerkki Json:</h2>
+                <code>
+                    {JSON.stringify(metaformJson, null, 2)}
+                </code>
+            </pre>
+        </Box>
+    </Grid>
+    <Grid item md={3} >
+ 
+    </Grid>
   </Grid>
+  
   </>
   );
 
